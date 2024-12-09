069dd98ba8c139acc22b7ea78cdeb21dc9f12a4e
Manhatai <matixxx.mp4@gmail.com>
2024-12-09 13:35:39 +0100

    final changes implemented

--- config/config.py
+++ config/config.py
@@ -4,3 +4,4 @@ __DB_BASE_HOST = f"{os.getenv('REST_API_DB_HOST')}:{os.getenv('REST_API_DB_PORT'
 __DB_CREDENTIALS = f"{os.getenv('REST_API_DB_LOGIN')}:{os.getenv('REST_API_DB_PASSWORD')}"
 SQLALCHEMY_DATABASE_URI = f'postgresql://{__DB_CREDENTIALS}@{__DB_BASE_HOST}/{os.getenv("REST_API_DB_NAME")}'
 SECRET_KEY = os.getenv("REST_API_SECRET_KEY")
+LOCAL_TOKEN = os.getenv("REST_API_LOCAL_TOKEN")
